6bf158926bb741d4ac3dd922f3162e4247f1b529
ahmet <ahmet.kasidecioglu@icloud.com>
2025-10-21 00:14:40 +0300

    player created and turns

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -11,12 +12,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y, PLAYER_RADIUS)
+    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
         screen.fill("black")
-        dt = clock.tick(60) / 1000
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(144) / 1000
        
     
     
